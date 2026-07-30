e32ab34ea6c560b5f48238d9c0f916120ef294f4
Pranima <pranimabhattarai9@gmail.com>
2026-07-30 14:16:42 +0545

    javascript

---
+++ learnJS/learn.js
@@ -0,0 +1,39 @@
+// words and keywords
+
+// variables constants and lets
+//    varible is to store DATA whose value changes eg. a = 1;
+//    constant is to store data whose value DOESN'T change eg. a = 1;
+//    let is to store TEMPORARY data whose value changes eg. a = 1;
+
+// hoisiting => using variable before its defined
+
+// let/const hoist huda = reference error aauxa, value set hudaina
+// var hoist huda = undefined aauxa
+
+// undefined and not-defined are different
+//    udefined is when you have the thing, it exist but the value isnt given, or you dont know
+//    not defined is where the thing doesnt exist
+
+// types in js
+//    primitives(data types) = number, string , null, undefined, boolean 
+//    and reference = [], (), {}
+//    
+
+
+// EXAMPLES
+
+// let age = 25;
+// console.log(age)
+
+// var x = 1
+// {
+//     var x = 2
+// }
+// console.log(x)
+
+// let a = 10
+// {
+//     let a = 12
+//     console.log(a)
+// }
+// console.log(a)
