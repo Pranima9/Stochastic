1e6eba2d1e4c07b08bef30ea9824cd679e268805
Pranima <pranimabhattarai9@gmail.com>
2026-08-05 13:47:55 +0545

    login page using tailwind

--- learnR/vite-project/src/App.jsx
+++ learnR/vite-project/src/App.jsx
@@ -1,13 +1,19 @@
-import Profile from "./MyComponents/profile";
+// import Profile from "./MyComponents/profile";
 import Hero from "./MyComponents/hero";
+import About from "./MyComponents/about";
+import Header from "./MyComponents/header";
+import Contact from "./MyComponents/contact";
+import Skill from "./MyComponents/skills";
+import LoginPage from "./MyComponents/LoginPage";
 
 function App() {
+    const num = "9801010101";
+    const email = "email@email";
     return(
         <>
-           <Profile/>
-           <Hero/>
-           <About/>
-           <kdfjgdbvsd/>
+           {/* <Profile/> */}
+
+         <LoginPage/>
         </>
     );
 }
