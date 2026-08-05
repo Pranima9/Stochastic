233ed5a5ed433fda49535952e3b1c1c5afb39a9c
Pranima <pranimabhattarai9@gmail.com>
2026-08-05 14:09:07 +0545

    image add

--- learnR/vite-project/src/App.jsx
+++ learnR/vite-project/src/App.jsx
@@ -1,19 +1,25 @@
 // import Profile from "./MyComponents/profile";
-import Hero from "./MyComponents/hero";
-import About from "./MyComponents/about";
-import Header from "./MyComponents/header";
-import Contact from "./MyComponents/contact";
-import Skill from "./MyComponents/skills";
+// import Hero from "./MyComponents/hero";
+// import About from "./MyComponents/about";
+// import Header from "./MyComponents/header";
+// import Contact from "./MyComponents/contact";
+// import Skill from "./MyComponents/skills";
+
 import LoginPage from "./MyComponents/LoginPage";
 
 function App() {
-    const num = "9801010101";
-    const email = "email@email";
     return(
         <>
            {/* <Profile/> */}
 
-         <LoginPage/>
+           {/* <Header/>
+           <Hero/>
+           <About/>
+           <Contact num={num} email={email}/>
+           <Skill/> */}
+
+           <LoginPage/>
+           
         </>
     );
 }
--- learnR/vite-project/src/MyComponents/LoginPage.jsx
+++ learnR/vite-project/src/MyComponents/LoginPage.jsx
@@ -1,17 +1,24 @@
 // import React from 'react'
+import backgroundImage from "../assets/blur.png"
 
 const LoginPage = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-        <form className="flex flex-col justify gap-2 justify-center w-[50vw] border border-blue-400  p-5 rounded-xl ">
+    <>
+    {/* <style>
+        backgroundImage
+    </style> */}
+    <div id="background" className="flex h-screen items-center justify-center ">
+        <form className="flex flex-col justify gap-2 justify-center w-[30vw] border border-blue-400  p-5 rounded-xl ">
+            {/* <img src="" alt="loading..." /> */}
             <h1 className="text-3xl font-bold text-red-500 flex mb-5">Login </h1>
             <input type="email" className="border-2 border-red-300 placeholder:text-sm p-1 rounded-md" placeholder="Enter your email address"/>
             <input type="password" className="border-2 border-red-300 placeholder:text-sm p-1 rounded-md" placeholder="Enter your password"/>
-            <button className="bg-green-400 text-white font-bold h-10 rounded-lg mt-3 hover:bg-green-550 transition-all"> Login </button>
+            <button className="bg-green-400 text-white font-bold h-10 rounded-lg mt-3 hover:bg-green-550"> Login </button>
             <button className="bg-blue-400 text-white font-bold h-10 rounded-lg ">Register</button>
             <a href="https://en.wikipedia.org/wiki/Hello,_world" className="text-gray-600 font-medium w-30 text-sm hover:underline transition-all duration-200 hover:translate-y-1">Forgot Password?</a>
         </form>
     </div>
+    </>
   )
 }
 
