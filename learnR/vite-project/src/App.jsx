28759c8b2147ad29a97593d82b5a8445283027ed
Pranima <pranimabhattarai9@gmail.com>
2026-08-06 13:25:17 +0545

    props understanding

--- learnR/vite-project/src/App.jsx
+++ learnR/vite-project/src/App.jsx
@@ -1,24 +1,25 @@
-// import Profile from "./MyComponents/profile";
-// import Hero from "./MyComponents/hero";
-// import About from "./MyComponents/about";
-// import Header from "./MyComponents/header";
-// import Contact from "./MyComponents/contact";
-// import Skill from "./MyComponents/skills";
+import Profile from "./MyComponents/profile";
+import Hero from "./MyComponents/hero";
+import About from "./MyComponents/about";
+import Header from "./MyComponents/header";
+import Contact from "./MyComponents/contact";
+import Skill from "./MyComponents/skills";
 
 import LoginPage from "./MyComponents/LoginPage";
 
 function App() {
     return(
         <>
-           {/* <Profile/> */}
+           <Profile/>
 
-           {/* <Header/>
+           <Header/>
            <Hero/>
            <About/>
-           <Contact num={num} email={email}/>
-           <Skill/> */}
+           <Contact/>
+           <Skill/>
 
            <LoginPage/>
+           
            
         </>
     );
