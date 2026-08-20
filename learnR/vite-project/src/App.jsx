470f1d120bd828dea10d96fae6f2ae381867dfab
Pranima <pranimabhattarai9@gmail.com>
2026-08-20 12:29:51 +0545

    count + - reset

--- learnR/vite-project/src/App.jsx
+++ learnR/vite-project/src/App.jsx
@@ -288,22 +288,32 @@
 
 // } export default App;
 
+
+
 // [[[[[[[[[ + - reset ]]]]]]]]]
 
 // import { useState } from "react";
 
-// function App(){
-
+// function App() {
 //   const [count, setCount] = useState(0);
 
-//   return(
+//   return (
 //     <>
-//       Count: {count}
+//       <h1>Count: {count}</h1>
+
+//       <button onClick={() => setCount(count + 1)}>
+//         +
+//       </button>
+
+//       <button onClick={() => setCount(count - 1)}>
+//         -
+//       </button>
+
+//       <button onClick={() => setCount(0)}>
+//         Reset
+//       </button>
 //     </>
 //   );
+// }
 
-// } export default App;
-
-
-
-// to manage
+// export default App;
